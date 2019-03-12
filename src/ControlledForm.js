6315ba5b78e6ad2f516e769b89c5d14f5645f281
Octowl <alsaff1987@gmail.com>
2019-03-12 11:48:09 +0300

    Up to successful sbumission in the backend

--- src/ControlledForm.js
+++ src/ControlledForm.js
@@ -1,10 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { connect } from "react-redux";
-import {} from "./redux/actions";
+import { submitPerson } from "./redux/actions";
 
-const ControlledForm = () => {
-  return <div></div>;
+const ControlledForm = ({ submitPerson }) => {
+  const [person, setPerson] = useState({
+    alias: "",
+    description: "",
+    email: "",
+  });
+
+  const handleChange = (e) =>
+    setPeron({ ...person, [e.target.name]: e.target.value });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    submitPerson(person);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text">Alias*</span>
+        </div>
+        <input
+          type="text"
+          className="form-control"
+          name="alias"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text">Description</span>
+        </div>
+        <input
+          type="text"
+          className="form-control"
+          name="description"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text">E-Mail*</span>
+        </div>
+        <input
+          type="email"
+          className="form-control"
+          name="email"
+          onChange={handleChange}
+        />
+      </div>
+      <input className="btn btn-outline-dark" type="submit" />
+    </form>
+  );
 };
 
 const mapStateToProps = (state) => {
@@ -12,7 +63,9 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    submitPerson: (person) => dispatch(submitPerson(person)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ControlledForm);
--- src/redux/actions/people.js
+++ src/redux/actions/people.js
@@ -1,15 +1,15 @@
-import { FETCH_PEOPLE } from "./actionTypes";
+import { FETCH_PEOPLE, ADD_PERSON } from "./actionTypes";
 
 import axios from "axios";
 
 export const fetchPeople = () => {
-  return async dispatch => {
+  return async (dispatch) => {
     try {
       const res = await axios.get("http://127.0.0.1:8000/alias/");
       const people = res.data;
       dispatch({
         type: FETCH_PEOPLE,
-        payload: people
+        payload: people,
       });
     } catch (error) {
       console.error("Something went wrong while fetching people");
@@ -18,4 +18,16 @@ export const fetchPeople = () => {
   };
 };
 
-export const submitPerson = data => {};
+export const submitPerson = (person) => {
+  return async (dispatch) => {
+    try {
+      const res = await axios.post("http://127.0.0.1:8000/alias/", person);
+      dispatch({
+        type: ADD_PERSON,
+      });
+    } catch (error) {
+      console.error("Person did not submit!");
+      console.error(error);
+    }
+  };
+};
