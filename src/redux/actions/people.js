2b9e91ba0b74e0b54f8d81f779443faab8a9865f
Octowl <alsaff1987@gmail.com>
2020-03-23 01:58:53 +0300

    cleanup syntax

--- src/redux/actions/people.js
+++ src/redux/actions/people.js
@@ -2,42 +2,38 @@ import { FETCH_PEOPLE, ADD_PERSON, SET_ERRORS } from "./actionTypes";
 
 import axios from "axios";
 
-export const fetchPeople = () => {
-  return async (dispatch) => {
-    try {
-      const res = await axios.get("http://127.0.0.1:8000/alias/");
-      const people = res.data;
-      dispatch({
-        type: FETCH_PEOPLE,
-        payload: people,
-      });
-    } catch (error) {
-      console.error("Something went wrong while fetching people");
-      console.error(error);
-    }
-  };
+export const fetchPeople = () => async (dispatch) => {
+  try {
+    const res = await axios.get("http://127.0.0.1:8000/alias/");
+    const people = res.data;
+    dispatch({
+      type: FETCH_PEOPLE,
+      payload: people,
+    });
+  } catch (error) {
+    console.error("Something went wrong while fetching people");
+    console.error(error);
+  }
 };
 
-export const submitPerson = (person, reset) => {
-  return async (dispatch) => {
-    try {
-      const res = await axios.post("http://127.0.0.1:8000/alias/", person);
-      const newPerson = res.data;
-      dispatch({
-        type: ADD_PERSON,
-        payload: newPerson,
-      });
-      dispatch({
-        type: SET_ERRORS,
-        payload: {},
-      });
-      reset();
-    } catch (error) {
-      console.error("Person did not submit!");
-      dispatch({
-        type: SET_ERRORS,
-        payload: error.response.data,
-      });
-    }
-  };
+export const submitPerson = (person, reset) => async (dispatch) => {
+  try {
+    const res = await axios.post("http://127.0.0.1:8000/alias/", person);
+    const newPerson = res.data;
+    dispatch({
+      type: ADD_PERSON,
+      payload: newPerson,
+    });
+    dispatch({
+      type: SET_ERRORS,
+      payload: {},
+    });
+    reset();
+  } catch (error) {
+    console.error("Person did not submit!");
+    dispatch({
+      type: SET_ERRORS,
+      payload: error.response.data,
+    });
+  }
 };
